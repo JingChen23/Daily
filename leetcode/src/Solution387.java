561af3ed5c259f9caa2dca2fbb0f7afa724f70ea
Chen <paulkim23@163.com>
2018-10-12 14:46:51 +0800

    [Issue #001] A great 5ms solution by using String.indexOf().

--- leetcode/src/Solution387.java
+++ leetcode/src/Solution387.java
@@ -1,23 +1,17 @@
-import java.util.HashSet;
-import java.util.Set;
-
 class Solution387 {
     public int firstUniqChar(String s) {
-        Set<Character> set1 = new HashSet<>();
-        Set<Character> set2 = new HashSet<>();
-        char[] array = s.toCharArray();
-        for(int i = 0; i < array.length; i++){
-            if (!set1.contains(array[i])){
-                set1.add(array[i]);
-            }
-            else{
-                set2.add(array[i]);
+        int len = s.length();
+        int lowest = len;
+
+        for (char i = 'a'; i <= 'z'; i++)   {
+            int first = s.indexOf(i);
+            if (first >= 0) {
+                if (lowest > first && s.lastIndexOf(i) == first)  {
+                    lowest = first;
+                }
             }
         }
-        for (int i = 0; i < array.length; i++){
-            if (!set2.contains(array[i])) return i;
-        }
-        return -1;
+        return lowest == len ? -1 : lowest;
     }
 
     public static void main(String[] args) {
